8caeb977e68e29cd0e38ff22868de53fab19f1ca
dravek99 <f.rakowskI@hotmail.com>
2015-11-24 00:06:26 +0100

    Dodane pliki

---
+++ src/texas/GameInfo.java
@@ -0,0 +1,29 @@
+package texas;
+
+public class GameInfo {
+	//liczba graczy bioracych udzial w rozgrywce
+	 private static int playersNumber;
+	 //aktualny posiadacz DealerButtona
+	 private static int dealerButtonOwner;
+	 //gracz rozpoczynajacy turê, tura koñczy siê w momencie przejscia do danego gracza
+	 private static int startingPlayer;
+	 public static int startingMoneyPerCapita;
+	 private static GameInfo gInfo = null;
+	 
+	 private GameInfo(int pNumber, int dButton, int money){
+		 playersNumber = pNumber;
+		 dealerButtonOwner = dButton;
+		 startingPlayer = dButton;
+		 startingMoneyPerCapita =  money/pNumber;
+	 }
+	 
+	 public static GameInfo getInstance(int pNumber, int dButton, int money) {
+		 if(gInfo == null) {
+	         gInfo = new GameInfo(pNumber, dButton, money);
+	      }
+	      return gInfo;  }
+	 
+	 public static int getPlayersNumber() {
+		 return playersNumber;
+	 }
+}
